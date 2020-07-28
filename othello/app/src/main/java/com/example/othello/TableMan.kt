b37fbadb2791915d8a74da856914d0e825555b29
kouichi-ogura <kouichi_ogura@sobal.co.jp>
2020-07-28 10:00:44 +0900

    use common defined

--- othello/app/src/main/java/com/example/othello/TableMan.kt
+++ othello/app/src/main/java/com/example/othello/TableMan.kt
@@ -3,19 +3,11 @@ package com.example.othello
 import androidx.appcompat.app.AppCompatActivity
 
 class TableMan {
-    // 定数
-    companion object {
-        const val Empty:Int = 0
-        const val Black:Int = 1
-        const val White:Int = 2
-        const val Blue:Int = 3  // 置ける場所のガイド用
-    }
-
     private var cellXnum:Int = 8    // X方向のマス数
     private var cellYnum:Int = 8    // Y方向のマス数
 
     // 盤面
-    public var board = Array(cellXnum) { IntArray(cellYnum){Empty} }
+    public var board = Array(cellXnum) { IntArray(cellYnum){common.CELL_EMPTY} }
         private set
 
     public fun Initialize( cellXnum:Int = 8, cellYnum:Int = 8 ){
@@ -24,15 +16,15 @@ class TableMan {
         this.cellYnum = cellYnum
 
         // 盤面の初期化
-        this.board = Array(cellXnum) {IntArray(cellYnum){Empty}}
+        this.board = Array(cellXnum) {IntArray(cellYnum){common.CELL_EMPTY}}
     }
 
     public fun InitialPlacement() {
         // 初期配置
-        PutStone(3, 3, Black)
-        PutStone(3, 4, White)
-        PutStone(4, 3, White)
-        PutStone(4, 4, Black)
+        PutStone(3, 3, common.CELL_BLACK)
+        PutStone(3, 4, common.CELL_WHITE)
+        PutStone(4, 3, common.CELL_WHITE)
+        PutStone(4, 4, common.CELL_BLACK)
     }
 
     public fun CountStone(color:Int):Int{
@@ -60,12 +52,12 @@ class TableMan {
 
     // 白石を置く
     public fun PutStoneWhite( cellX:Int, cellY:Int ):Boolean{
-        return PutStone(cellX, cellY, White)
+        return PutStone(cellX, cellY, common.CELL_WHITE)
     }
 
     // 黒石を置く
     public fun PutStoneBlack( cellX:Int, cellY:Int ):Boolean{
-        return PutStone(cellX, cellY, Black)
+        return PutStone(cellX, cellY, common.CELL_BLACK)
     }
 
     // Debug用途
