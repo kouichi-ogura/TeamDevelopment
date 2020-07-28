e6d28b07cc18aeca63d4b9bfa161c9d6b4d2b3a8
kouichi-ogura <kouichi_ogura@sobal.co.jp>
2020-07-28 21:20:24 +0900

    review fixed

--- othello/app/src/main/java/com/example/othello/TableMan.kt
+++ othello/app/src/main/java/com/example/othello/TableMan.kt
@@ -1,7 +1,5 @@
 package com.example.othello
 
-import androidx.appcompat.app.AppCompatActivity
-
 class TableMan {
     private var cellXnum:Int = 8    // X方向のマス数
     private var cellYnum:Int = 8    // Y方向のマス数
@@ -10,7 +8,7 @@ class TableMan {
     public var board = Array(cellXnum) { IntArray(cellYnum){common.CELL_EMPTY} }
         private set
 
-    public fun Initialize( cellXnum:Int = 8, cellYnum:Int = 8 ){
+    public fun initialize( cellXnum:Int = 8, cellYnum:Int = 8 ){
         // 盤面のサイズ決定
         this.cellXnum = cellXnum
         this.cellYnum = cellYnum
@@ -19,30 +17,30 @@ class TableMan {
         this.board = Array(cellXnum) {IntArray(cellYnum){common.CELL_EMPTY}}
     }
 
-    public fun InitialPlacement() {
+    public fun initialPlacement() {
         // 初期配置
-        PutStone(3, 3, common.CELL_BLACK)
-        PutStone(3, 4, common.CELL_WHITE)
-        PutStone(4, 3, common.CELL_WHITE)
-        PutStone(4, 4, common.CELL_BLACK)
+        putStone(3, 3, common.CELL_BLACK)
+        putStone(3, 4, common.CELL_WHITE)
+        putStone(4, 3, common.CELL_WHITE)
+        putStone(4, 4, common.CELL_BLACK)
     }
 
-    public fun CountStone(color:Int):Int{
-        var StoneNum = 0
+    public fun countStone(color:Int):Int{
+        var stoneNum = 0
 
         for (i in 0..7) {
             for (j in 0..7) {
                 if ( board[i][j] == color ){
                     println("${i} ${j} ${color}")
-                    StoneNum ++
+                    stoneNum ++
                 }
             }
         }
-        return StoneNum
+        return stoneNum
     }
 
     // 色指定で石を置く
-    public fun PutStone( cellX:Int, cellY:Int, color:Int ):Boolean{
+    public fun putStone( cellX:Int, cellY:Int, color:Int ):Boolean{
         if ( cellXnum <= cellX ) return false
         if ( cellYnum <= cellY ) return false
 
@@ -51,17 +49,17 @@ class TableMan {
     }
 
     // 白石を置く
-    public fun PutStoneWhite( cellX:Int, cellY:Int ):Boolean{
-        return PutStone(cellX, cellY, common.CELL_WHITE)
+    public fun putStoneWhite( cellX:Int, cellY:Int ):Boolean{
+        return putStone(cellX, cellY, common.CELL_WHITE)
     }
 
     // 黒石を置く
-    public fun PutStoneBlack( cellX:Int, cellY:Int ):Boolean{
-        return PutStone(cellX, cellY, common.CELL_BLACK)
+    public fun putStoneBlack( cellX:Int, cellY:Int ):Boolean{
+        return putStone(cellX, cellY, common.CELL_BLACK)
     }
 
     // Debug用途
-    public fun ExportBoard(){
+    public fun exportBoard(){
         for (i in 0..7) {
             for (j in 0..7) {
                 print("${board[i][j]} ")
