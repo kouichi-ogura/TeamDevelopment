9b4a5bebb8c2e0ffd74a84d0af94b13a9780ed8b
kouichi-ogura <kouichi_ogura@sobal.co.jp>
2020-07-31 09:33:33 +0900

    cord sharing

--- othello/app/src/main/java/com/example/othello/TableMan.kt
+++ othello/app/src/main/java/com/example/othello/TableMan.kt
@@ -4,36 +4,34 @@ class TableMan {
     private var cellXnum:Int = 8    // X方向のマス数
     private var cellYnum:Int = 8    // Y方向のマス数
 
-    public fun DeubugBoard(){
-        this.board = Array(cellXnum) {IntArray(cellYnum){Common.CELL_WHITE}}
-        putStone(0, 0, Common.CELL_EMPTY)
-        putStone(0, 2, Common.CELL_BLACK)
-    }
-
-    // 盤面
-    public var board = Array(cellXnum) { IntArray(cellYnum){Common.CELL_EMPTY} }
+    // 盤面の配列
+    var board = Array(cellXnum) { IntArray(cellYnum){Common.CELL_EMPTY} }
         private set
 
-    public fun initialize( cellXnum:Int = 8, cellYnum:Int = 8 ){
+    // 初期化
+    fun initialize( cellXnum:Int = 8, cellYnum:Int = 8 ){
         // 盤面のサイズ決定
         this.cellXnum = cellXnum
         this.cellYnum = cellYnum
 
         // 盤面の初期化
         this.board = Array(cellXnum) {IntArray(cellYnum){Common.CELL_EMPTY}}
-
-        // DeubugBoard()
     }
 
-    public fun initialPlacement() {
+    // 初期配置
+    fun initialPlacement() {
         // 初期配置
         putStone(3, 3, Common.CELL_BLACK)
         putStone(3, 4, Common.CELL_WHITE)
         putStone(4, 3, Common.CELL_WHITE)
         putStone(4, 4, Common.CELL_BLACK)
+
+        // デバッグ用
+        // deubugBoard()
     }
 
-    public fun countStone(color:Int):Int{
+    // 石の数を数える
+    fun countStone(color:Int):Int{
         var stoneNum = 0
 
         for (i in 0..7) {
@@ -46,8 +44,18 @@ class TableMan {
         return stoneNum
     }
 
+    // 白石を置く
+    fun putStoneWhite( cellX:Int, cellY:Int ):Boolean{
+        return putStone(cellX, cellY, Common.CELL_WHITE)
+    }
+
+    // 黒石を置く
+    fun putStoneBlack( cellX:Int, cellY:Int ):Boolean{
+        return putStone(cellX, cellY, Common.CELL_BLACK)
+    }
+
     // 色指定で石を置く
-    public fun putStone( cellX:Int, cellY:Int, color:Int ):Boolean{
+    fun putStone( cellX:Int, cellY:Int, color:Int ):Boolean{
         if ( cellXnum <= cellX ) return false
         if ( cellYnum <= cellY ) return false
 
@@ -55,18 +63,15 @@ class TableMan {
         return true
     }
 
-    // 白石を置く
-    public fun putStoneWhite( cellX:Int, cellY:Int ):Boolean{
-        return putStone(cellX, cellY, Common.CELL_WHITE)
+    // Debug：盤面をレイアウト
+    fun deubugBoard(){
+        this.board = Array(cellXnum) {IntArray(cellYnum){Common.CELL_WHITE}}
+        putStone(0, 0, Common.CELL_EMPTY)
+        putStone(0, 2, Common.CELL_BLACK)
     }
 
-    // 黒石を置く
-    public fun putStoneBlack( cellX:Int, cellY:Int ):Boolean{
-        return putStone(cellX, cellY, Common.CELL_BLACK)
-    }
-
-    // Debug用途
-    public fun exportBoard(){
+    // Debug:盤面の情報を出力
+    fun debugExportBoard(){
         for (i in 0..7) {
             for (j in 0..7) {
                 print("${board[i][j]} ")
